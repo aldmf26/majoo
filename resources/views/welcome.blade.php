7b6d3a070b3737d137c1b86846703972c94aa795
aldmf26 <aldimf26@gmail.com>
2025-07-09 09:31:03 +0800

    dsa

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,7 +7,6 @@
 	<meta name="author" content="" />
 	<title>PILIH MENU TS</title>
 	<link rel="icon" type="image/x-icon" href="assets/img/favicon.ico" />
-	<script src="https://use.fontawesome.com/releases/v5.15.1/js/all.js" crossorigin="anonymous"></script>
 	<link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css" />
 	<link href="https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet" type="text/css" />
 	<link href="{{ asset('assets') }}/ptagafood/menu/css/styles.css" rel="stylesheet" />
